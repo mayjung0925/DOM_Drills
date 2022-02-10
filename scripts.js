66e55f8affce5c0aeb3023312d3071d71a91094b
Eunjin Jeoung <mayjung0925@gmail.com>
2022-02-10 11:32:32 -0600

    Added setRandomColor function

--- scripts.js
+++ scripts.js
@@ -3,82 +3,80 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    let header = document.createElement("div");
+    const header = document.createElement("div");
     header.className = 'header-container';
 
-    let title1 = document.createElement('h1');
-    let title1Text = document.createTextNode('This is an h1')
+    const title1 = document.createElement('h1');
+    const title1Text = document.createTextNode('This is an h1')
     title1.className = 'h1';
 
+    const title2 = document.createElement('h2');
+    const title2Text = document.createTextNode('This is an h2')
+    title2.classList.add('h2')
+
+
+    const title3 = document.createElement('h3');
+    const title3Text = document.createTextNode('This is an h3')
+    title3.className = 'h3';
+
+    const title4 = document.createElement('h4');
+    const title4Text = document.createTextNode('This is an h4');
+    title4.className = 'h4';
+
+    const title5 = document.createElement('h5');
+    const title5Text = document.createTextNode("This is an h5");
+    title5.className = 'h5';
+
+    const title6 = document.createElement('h6');
+    const title6Text = document.createTextNode('This is an h6');
+    title6.className = 'h6';
 
     title1.appendChild(title1Text);
     header.appendChild(title1);
     document.body.appendChild(header);
 
-
-
-    let title2 = document.createElement('h2');
-    let title2Text = document.createTextNode('This is an h2')
-    title2.className = 'h2'
-
-
     title2.appendChild(title2Text);
     header.appendChild(title2);
-
-
-    let title3 = document.createElement('h3');
-    let title3Text = document.createTextNode('This is an h3')
-    title3.className = 'h3';
 
     title3.appendChild(title3Text);
     header.appendChild(title3);
 
-    let title4 = document.createElement('h4');
-    let title4Text = document.createTextNode('This is an h4');
-    title4.className = 'h4';
-
     title4.appendChild(title4Text);
     header.appendChild(title4);
-
-    let title5 = document.createElement('h5');
-    let title5Text = document.createTextNode("This is an h5");
-    title5.className = 'h5';
 
     title5.appendChild(title5Text);
     header.appendChild(title5);
 
-    let title6 = document.createElement('h6');
-    let title6Text = document.createTextNode('This is an h6');
-    title6.className = 'h6';
-
     title6.appendChild(title6Text);
     header.appendChild(title6);
 
-    const colors = ['black', 'blue', 'red', 'green', 'yellow', 'gray', 'coral', 'pink']
+    title1.addEventListener('click', setRandomColor);
+    title2.addEventListener('click', setRandomColor);
+    title3.addEventListener('click', setRandomColor);
+    title4.addEventListener('click', setRandomColor);
+    title5.addEventListener('click', setRandomColor);
+    title6.addEventListener('click', setRandomColor);
 
-    title1.addEventListener('dblclick', function () {
-        title1.style.color = colors[Math.floor(Math.random() * 8)]
-    })
+    // Naming arrary cleary by adding 'Arr'
 
-    title2.addEventListener('dblclick', function () {
-        title2.style.color = colors[Math.floor(Math.random() * 8)]
-    })
+    const colorArr = [
+        'black',
+        'blue',
+        'red',
+        'green',
+        'yellow',
+        'gray',
+        'coral',
+        'pink'
+    ]
 
-    title3.addEventListener('dblclick', function () {
-        title3.style.color = colors[Math.floor(Math.random() * 8)]
-    })
+    // getting a random number by using Math.floor(Math.random()* )
+    // event.target indicates the current element
+    function setRandomColor(event) {
+        const randomNum = Math.floor(Math.random() * colorArr.length);
+        event.target.style.color = colorArr[randomNum];
+    }
 
-    title4.addEventListener('dblclick', function () {
-        title4.style.color = colors[Math.floor(Math.random() * 8)]
-    })
-
-    title5.addEventListener('dblclick', function () {
-        title5.style.color = colors[Math.floor(Math.random() * 8)]
-    })
-
-    title6.addEventListener('dblclick', function () {
-        title6.style.color = colors[Math.floor(Math.random() * 8)]
-    })
 
     let button = document.createElement('button');
     let btnText = document.createTextNode('Click to add new list item');
@@ -87,8 +85,9 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.appendChild(button);
 
     let countClicks = 0;
-
-    function listItem() {
+    // increment a number manually by setting a variable countClicks/ countClickts++ 
+    //Arranging the work flow is important
+    function listItem(event) {
 
         countClicks++
         let list = document.createElement('li');
@@ -98,18 +97,12 @@ document.addEventListener('DOMContentLoaded', function () {
         list.appendChild(listText);
         document.body.appendChild(list);
 
-        list.addEventListener('click', function () {
-
-            list.style.color = colors[Math.floor(Math.random() * 8)]
-
-        })
+        list.addEventListener('click', setRandomColor)
 
         list.addEventListener('dblclick', function () {
             list.remove();
 
         })
-
-
 
     }
 
